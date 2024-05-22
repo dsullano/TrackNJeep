b1c98e6b7d93d2a420e6c8317a8c9c30cf392d3d
LAB11_USER ON WS13 <L11X13W13@citulab.net>
2024-05-22 14:02:21 +0800

    fixed

--- app/src/main/java/com/example/tracknjeep_test/notification.java
+++ app/src/main/java/com/example/tracknjeep_test/notification.java
@@ -9,6 +9,6 @@ public class notification extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification);
+       // setContentView(R.layout.activity_notification);
     }
 }
