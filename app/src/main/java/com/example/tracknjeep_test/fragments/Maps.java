30278ee0626d1dc24055421eebaffd1cc6e4758f
Ceven <valmykelcevenbolante@gmail.com>
2024-05-25 13:31:26 +0800

    did some fix

--- app/src/main/java/com/example/tracknjeep_test/fragments/Maps.java
+++ app/src/main/java/com/example/tracknjeep_test/fragments/Maps.java
@@ -1,11 +1,13 @@
 package com.example.tracknjeep_test.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 
 import com.example.tracknjeep_test.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,47 +17,36 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Maps extends Fragment {
+public class Maps extends Fragment implements OnMapReadyCallback {
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private GoogleMap mMap;
 
-        if (getActivity() != null) {
-            // Launch MapsActivity
-            Intent intent = new Intent(this.getActivity(), MapsActivity.class);
-            startActivity(intent);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
 
-            // Remove this fragment after starting the activity
-            getActivity().getSupportFragmentManager().beginTransaction().remove(this).commitAllowingStateLoss();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map_container);
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance();
+            getChildFragmentManager().beginTransaction()
+                    .replace(R.id.map_container, mapFragment)
+                    .commit();
         }
+        mapFragment.getMapAsync(this);
+
+        return view;
     }
 
-    public static class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-        private GoogleMap mMap;
-
-        @Override
-        protected void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_maps);
-
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map);
-            if (mapFragment != null) {
-                mapFragment.getMapAsync(this);
-            }
-        }
-
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            mMap = googleMap;
-
-            // Add a marker and move the camera
-            LatLng citu = new LatLng(10.29441995166696, 123.8811194524);
-            mMap.addMarker(new MarkerOptions().position(citu).title("CIT-U"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(citu, 15));
-        }
+        // Add a marker and move the camera
+        LatLng citu = new LatLng(10.29441995166696, 123.8811194524);
+        mMap.addMarker(new MarkerOptions().position(citu).title("CIT-U"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(citu, 15));
     }
 }
